b522ef0646cc6a954f4a7e88de21ac9fa61e17e0
maximillian <maximilliaer@gmail.com>
2021-01-25 21:29:40 +0500

    1 commit

---
+++ crud-module/src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+  departments(name, address)
+  VALUES ('IBM', 'USA');
+INSERT INTO
+  departments(name, address)
+  VALUES ('Sber', 'Russia, kuibisheva street');
+INSERT INTO
+  departments(name, address)
+  VALUES ('Apple', 'USA, Kalifornia');
+
+INSERT INTO
+  workers(name, last_name, position, department_id, birthday, salary)
+  VALUES('Denis', 'Medvedev', 'Developer', 3, '1992-03-18T01:30:00Z', 120000);
+INSERT INTO
+  workers(name, last_name, position, department_id, birthday, salary)
+  VALUES('Maxim', 'Eliseev', 'junior-Developer', 2, '1987-06-08T12:30:00Z', 70000);
+INSERT INTO
+  workers(name, last_name, position, department_id, birthday, salary)
+  VALUES('Anton', 'Kilanov', 'Senior-Developer', 3, '1981-07-24T19:55:00Z', 223000);
